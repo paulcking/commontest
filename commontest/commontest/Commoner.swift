53d6c9032f5ff641f17d60f32c11a01c1b864d67
pck <pck@pckmacbookpro.attlocal.net>
2017-01-15 23:45:55 -0500

    added functionality

---
+++ commontest/commontest/Commoner.swift
@@ -0,0 +1,16 @@
+//
+//  Commoner.swift
+//  commontest
+//
+//  Created by Paul King on 1/15/17.
+//  Copyright © 2017 VNP. All rights reserved.
+//
+
+import Foundation
+
+class Commoner {
+    
+    static func sayHello(who: String) -> String {
+        return "hello, \(who)"
+    }
+}
