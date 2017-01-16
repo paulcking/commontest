6e12b4e40b47d0de3d7c50bcc71273ffe23ae347
pck <pck@pckmacbookpro.attlocal.net>
2017-01-15 23:02:24 -0500

    add empty framework

---
+++ commontest/commontest/commontest.h
@@ -0,0 +1,19 @@
+//
+//  commontest.h
+//  commontest
+//
+//  Created by Paul King on 1/15/17.
+//  Copyright © 2017 VNP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for commontest.
+FOUNDATION_EXPORT double commontestVersionNumber;
+
+//! Project version string for commontest.
+FOUNDATION_EXPORT const unsigned char commontestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <commontest/PublicHeader.h>
+
+
